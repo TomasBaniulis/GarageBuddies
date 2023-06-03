cb1923ab7b8a3cfb3eb59f896a87d03a4204c9ea
unknown <baniulis.tomas@gmail.com>
2023-06-03 23:45:04 +0300

    init commit

---
+++ GarageBuddies-api/src/main/java/lt/code/academy/garagebuddiesapi/GarageBuddiesApiApplication.java
@@ -0,0 +1,13 @@
+package lt.code.academy.garagebuddiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageBuddiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageBuddiesApiApplication.class, args);
+	}
+
+}
---
+++ GarageBuddies-api/src/test/java/lt/code/academy/garagebuddiesapi/GarageBuddiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.code.academy.garagebuddiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageBuddiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
